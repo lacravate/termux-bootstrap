40e8cd912108d4938109b1047f7d8a02c735b402
lacravate <lacravate@lacravate.fr>
2017-08-21 01:24:09 +0200

    install.sh; installs bootstrap to termux

---
+++ install/install.sh
@@ -0,0 +1,6 @@
+install_dir=$(echo $PATH|tr ':' "\n"|grep com.termux|sort|head -1)
+executable_dir=$(dirname $0)
+
+for file in $(ls $executable_dir/* | grep -v install.sh); do
+  cp $file $install_dir
+done
